46472dd68c64e50ebad95ef5d52f608d0493fc2c
Inglume <inglume@gmail.com>
2019-05-14 22:51:53 -0400

    Did some of assignment 8

--- Picture/src/Picture.java
+++ Picture/src/Picture.java
@@ -321,6 +321,37 @@ public class Picture extends SimplePicture {
     this.write("collage.jpg");
   }
 
+  /** Method to create a collage of several pictures with extra params */
+  public void createCollage(int startRow, int endRow, int startCol, int endCol) {
+    Picture flower1 = new Picture("flower1.jpg");
+    Picture flower2 = new Picture("flower2.jpg");
+    this.copy(flower1, 0, 0);
+    this.copy(flower2, 100, 0);
+    this.copy(flower1, 200, 0);
+    Picture flowerNoBlue = new Picture(flower2);
+    flowerNoBlue.zeroBlue();
+    this.copy(flowerNoBlue, 300, 0);
+    this.copy(flower1, 400, 0);
+    this.copy(flower2, 500, 0);
+    this.mirrorVertical();
+    this.write("collage.jpg");
+  }
+
+  /** Method to create my collage of several pictures */
+  public void myCollage() {
+    Picture flower1 = new Picture("flower1.jpg");
+    Picture flower2 = new Picture("flower2.jpg");
+    this.copy(flower1, 0, 0);
+    this.copy(flower2, 100, 0);
+    this.copy(flower1, 200, 0);
+    Picture flowerNoBlue = new Picture(flower2);
+    flowerNoBlue.zeroBlue();
+    this.copy(flowerNoBlue, 300, 0);
+    this.copy(flower1, 400, 0);
+    this.copy(flower2, 500, 0);
+    this.mirrorVertical();
+    this.write("collage.jpg");
+  }
 
   /**
    * Method to show large changes in color
