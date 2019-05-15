8b4179a9d6da02929ca240c74b89ecfd3dc86863
Inglume <inglume@gmail.com>
2019-05-14 21:22:08 -0400

    Finished

--- Picture/src/IntArrayWorker.java
+++ Picture/src/IntArrayWorker.java
@@ -1,94 +1,127 @@
-public class IntArrayWorker
-{
+public class IntArrayWorker {
   /** two dimensional matrix */
   private int[][] matrix = null;
-  
-  /** set the matrix to the passed one
-    * @param theMatrix the one to use
-    */
-  public void setMatrix(int[][] theMatrix)
-  {
+
+  /**
+   * set the matrix to the passed one
+   * 
+   * @param theMatrix the one to use
+   */
+  public void setMatrix(int[][] theMatrix) {
     matrix = theMatrix;
   }
-  
+
   /**
-   * Method to return the total 
+   * Method to return the occurences of target
+   * 
+   * @return the occurences of target
+   */
+  public int getCount(int target) {
+    int count = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] == target) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Method to return the largest
+   * 
+   * @return the largest
+   */
+  public int getLargest() {
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[i].length; j++) {
+        if (matrix[i][j] > max) {
+          max = matrix[i][j];
+        }
+      }
+    }
+    return max;
+  }
+
+  /**
+   * Method to return the total in a column
+   * 
+   * @return the total
+   */
+  public int getColTotal(int col) {
+    int total = 0;
+    for (int i = 0; i < matrix.length; i++) {
+      total += matrix[i][col];
+    }
+    return total;
+  }
+
+  /**
+   * Method to return the total
+   * 
    * @return the total of the values in the array
    */
-  public int getTotal()
-  {
+  public int getTotal() {
     int total = 0;
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length; col++) {
         total = total + matrix[row][col];
       }
     }
     return total;
   }
-  
+
   /**
    * Method to return the total using a nested for-each loop
+   * 
    * @return the total of the values in the array
    */
-  public int getTotalNested()
-  {
+  public int getTotalNested() {
     int total = 0;
-    for (int[] rowArray : matrix)
-    {
-      for (int item : rowArray)
-      {
+    for (int[] rowArray : matrix) {
+      for (int item : rowArray) {
         total = total + item;
       }
     }
     return total;
   }
-  
+
   /**
    * Method to fill with an increasing count
    */
-  public void fillCount()
-  {
+  public void fillCount() {
     int numCols = matrix[0].length;
     int count = 1;
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < numCols; col++)
-      {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < numCols; col++) {
         matrix[row][col] = count;
         count++;
       }
     }
   }
-  
+
   /**
    * print the values in the array in rows and columns
    */
-  public void print()
-  {
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; col++)
-      {
-        System.out.print( matrix[row][col] + " " );
+  public void print() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length; col++) {
+        System.out.print(matrix[row][col] + " ");
       }
       System.out.println();
     }
     System.out.println();
   }
-  
-  
-  /** 
+
+
+  /**
    * fill the array with a pattern
    */
-  public void fillPattern1()
-  {
-    for (int row = 0; row < matrix.length; row++)
-    {
-      for (int col = 0; col < matrix[0].length; 
-           col++)
-      {
+  public void fillPattern1() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[0].length; col++) {
         if (row < col)
           matrix[row][col] = 1;
         else if (row == col)
@@ -98,5 +131,5 @@ public class IntArrayWorker
       }
     }
   }
- 
+
 }
